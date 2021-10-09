211b361e833668a8d2c1da32fdb59d42c7b04f42
marcos_antonio <marcospinheiro360@gmail.com>
2021-10-09 00:40:14 -0300

    ajustes do deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,34 @@ import { Module } from '@nestjs/common';
 import { LegalTechnicianModule } from './legal-technician/legal-technician.module';
 import { ProcessModule } from './process/process.module';
 import { TypeOrmModule} from '@nestjs/typeorm';
-import { LegalTechnicianService } from './legal-technician/legal-technician.service';
 import { AuthModule } from './auth/auth.module';
 import { WorkInProcessModule } from './work-in-process/work-in-process.module';
+import { AppController } from './app/app.controller';
+
 
 
 @Module({
   imports: [
   TypeOrmModule.forRoot({
+    name:"default",
     type:"postgres",
     host: 'ec2-54-172-169-87.compute-1.amazonaws.com',
     port: 5432,
     username: 'itaajdbmjdpgls',
     password: 'bb403b948655d827712d6f7203ec8804016adbc2b575f3106fdf750e459577ea',
     database: 'datik56us3qjh8',
+    extra:{
+      ssl:{
+        rejectUnauthorized: false
+      }
+    },
     entities: [__dirname +'/**/*.entity{.js,.ts}'],
     synchronize: true,
   }),
   LegalTechnicianModule, 
   ProcessModule, AuthModule, WorkInProcessModule
 ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
